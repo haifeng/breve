9aafbf737deb5079a1fbebf64cba97fe7d28ef61
Juris Galang <jgalang@hanzo.epicentercafe.com>
2009-09-25 14:29:59 -0700

    raise delivery errors on mailer when in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,8 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # ActiveMailer settings
-config.action_mailer.perform_deliveries    = true
 config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries    = true
 config.action_mailer.default_charset       = "iso-8859-1"
 config.action_mailer.delivery_method       = :sendmail
 config.action_mailer.sendmail_settings = {
