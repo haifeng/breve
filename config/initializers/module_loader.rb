a17a6b981fe4e90305bc32cd762dcf3d2ecceb0b
Juris Galang <jgalang@hanzo.local>
2009-09-24 18:18:06 -0700

    Notes added

--- config/initializers/module_loader.rb
+++ config/initializers/module_loader.rb
@@ -1,4 +1,9 @@
-# Dependencies
+# NOTE: we load the dependency modules from the lib/ via an initializer
+# instead of via the environment.rb because otherwise it wont be loaded
+# when we deploy to Heroku.
+#
+# Ideally, these should be turned into a plugin or packaged as a Gem
+#
 require 'acts_as_voter'
 require 'acts_as_post'
 require 'extensions'
