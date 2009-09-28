7ce74f46fb3ad168dc830ce32465a1a930bfb67d
Juris Galang <jgalang@hanzo.local>
2009-09-28 10:50:43 -0700

    Moved out fake seed task to a different task file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,32 +9,3 @@
 Section.create([ 
   { :name => 'Breve', :description => 'RESERVED. This is the default section used for posts.' }
 ])
-
-## dummy users 
-users = User.create([ 
-  { :email => 'juris@breve.com',    :password => 'password', :lastname => 'galang', :firstname => 'juris'   },  
-  { :email => 'kreek`t@breve.com',  :password => 'password', :lastname => 'rebaño', :firstname => 'kreek`t' },  
-  { :email => 'myrhon@breve.com',   :password => 'password', :lastname => 'galang', :firstname => 'myrhon'  },  
-  { :email => 'jared@breve.com',    :password => 'password', :lastname => 'galang', :firstname => 'jared'   },  
-  { :email => 'jace@breve.com',     :password => 'password', :lastname => 'galang', :firstname => 'jace'    },  
-  { :email => 'justin@breve.com',   :password => 'password', :lastname => 'galang', :firstname => 'justin'  },  
-  { :email => 'jaden@breve.com',    :password => 'password', :lastname => 'galang', :firstname => 'jaden'   }  
-])
-
-# dummy articles
-(rand(10) + 5).times do |i|
-  users.each do |user|
-    post = Post.new(
-      :author  => user,
-      :points  => rand(10) + 1,
-      :url     => "http://www.post#{i}.com", 
-      :title   => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    )
-    post.save!
-  end
-end
-
-Post.find(:all).each do |post|
-  Post.destroy(post) if post.author.nil?
-end
-
