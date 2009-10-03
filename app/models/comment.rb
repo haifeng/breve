f9fe9d5eb5992bf3c2c2c8190c263d057e74eac3
Juris Galang <jgalang@hanzo.local>
2009-10-02 17:13:54 -0700

    destroy votes when post or comments are deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,7 @@ class Comment < ActiveRecord::Base
   belongs_to :author, :counter_cache => true, 
     :class_name => 'User', :foreign_key => 'user_id'
   
-  has_many :votes,    :as => :voteable
+  has_many :votes, :as => :voteable, :dependent => :destroy
   has_many :comments, :as => :commentable
 
   alias :replies :comments
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ActiveRecord::Base
   belongs_to :author, :counter_cache => true, 
     :class_name => 'User', :foreign_key => 'user_id'
   
-  has_many :votes,    :as => :voteable
+  has_many :votes, :as => :voteable, :dependent => :destroy
   has_many :comments, :as => :commentable
 
   before_save :normalize_field_values
