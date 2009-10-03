18f72181b881651c17595e0b02ded86ff35935f5
Juris Galang <jgalang@hanzo.local>
2009-10-02 17:21:30 -0700

    fixed voting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -8,15 +8,10 @@ class Vote < ActiveRecord::Base
   def self.cast_for(user, post)
     # prevent user from voting posts it owns
     return if post.author == user
-    
-    puts "xx #{user.id} #{user.displayname}  #{post.id} #{post.headline} #{post.class.name}" 
 
     # toggle user's casted votes for the target post
     # first check if the user has already voted on this post
     vote = self.find_by_user_id_and_voteable_id_and_voteable_type(user, post, post.class.name) 
-    unless vote.nil?
-      puts ">> #{vote.user_id} #{vote.voteable_id} #{vote.voteable_type}" 
-    end
     
     # if not, then create a new vote
     if vote.nil?
